2213b446642597a4ac974b9243da94b93337fd6a
Audrey Josephine <audrey.josephine35@gmail.com>
2024-05-08 20:13:29 +0700

    changes in backend

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   outputDir: 'frontend', // Output directory for Vue build
   devServer: {
+    // disableHostCheck: true,
+    allowedHosts: [
+      'localhost',
+      '.ngrok-free.app',
+      '.ngrok.io',
+    ],
     proxy: {
       '/absen': {
         target: 'http://127.0.0.1:5000', // Target URL of Flask backend
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from backend import create_app
+from app import create_app
 import os
 import threading
 
